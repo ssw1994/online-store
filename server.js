b1c1a8e21f0e382a4b11fa9314790342bb77db97
sachin <sachin.waghmare@standardbank.co.za>
2022-12-13 22:36:07 +0530

    added server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import ENVIRONMENT from "dotenv";
-
+//const router = express.Router();
+//import http from "http";
 ENVIRONMENT.config();
 
 const app = express();
@@ -17,6 +18,35 @@ mongoose
   .catch((error) => {
     //console.log("Error in connected to db ", error);
   });
+
+const persons = [
+  {
+    name: "sachin",
+    age: 28,
+  },
+  {
+    name: "sagar",
+    age: 30,
+  },
+];
+
+app.get("/persons", (req, res) => {
+  const { name, age } = req.query;
+  console.log(name, age);
+  if (req.query.hasOwnProperty("age") || req.query.hasOwnProperty("name")) {
+    res.send(persons.filter((p) => p.name == name || p.age == age));
+  } else {
+    res.send(persons);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+// http.createServer(app).listen(port, () => {
+//   console.log("Server started on PORT : ", port);
+// });
 
 app.listen(port, () => {
   console.log("Server started on PORT : ", port);
---
+++ src/tutorials/RestApis/RestApis.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+import React from "react";
+
+/**
+ * @author
+ * @function RestApi
+ **/
+
+export const RestApi = (props) => {
+  return <div></div>;
+};
